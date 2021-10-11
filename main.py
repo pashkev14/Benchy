c90d09a26bee82f84f8aae497b2dd113f8f4f10a
pashkevich <pavsav.04@gmail.com>
2021-10-11 20:10:00 +0500

    created base window

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+from PySide6 import QtWidgets, QtCore
+import sys
+
+
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.resize(400, 300)
+
+        self.text = QtWidgets.QLabel("Hello, world!", alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+
+
 if __name__ == '__main__':
-    print("Hello, world!")
+    app = QtWidgets.QApplication([])
+
+    window = MainWindow()
+    window.setWindowTitle('Hello')
+    window.show()
+    sys.exit(app.exec())
